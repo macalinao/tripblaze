74c9398fcca35f7a714ac07128a886684672e179
Ian Macalinao <ian@giza.us>
2014-08-01 19:50:46 -0500

    Add right column
    Signed-off-by: Ian Macalinao <ian@giza.us>

--- client/app/create/create.controller.js
+++ client/app/create/create.controller.js
@@ -12,8 +12,11 @@ angular.module('tripmakerApp')
       zoom: 10
     };
 
+    $scope.currentDay = 1;
+    $scope.days = [1, 2, 3, 4, 5];
+
     $scope.pois = [];
-    $http.get('/pois/Paris').then(function(data) {
+    $http.get('/pois/Orlando').then(function(data) {
       $scope.pois = data.data.pois;
       $scope.map.center.latitude = data.data.loc.lat;
       $scope.map.center.longitude = data.data.loc.lng;
