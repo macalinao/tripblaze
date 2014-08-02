68b9b7513a52fe14144eafa479471b890ad6da79
Ian Macalinao <ian@giza.us>
2014-08-01 22:15:51 -0500

    Order by distance
    Signed-off-by: Ian Macalinao <ian@giza.us>

--- client/app/create/create.controller.js
+++ client/app/create/create.controller.js
@@ -21,14 +21,52 @@ angular.module('tripmakerApp')
 
     $scope.pois = [];
 
-    $scope.addCurrentDay = function(data, event) {
+    $scope.getCurPois = function() {
       var its = $scope.itineraries;
       var cur = $scope.currentDay;
 
       var curPois = its[cur];
       if (!curPois) {
         curPois = its[cur] = [];
-      } else {
+      }
+      return curPois;
+    };
+
+    $scope.updateDistances = function() {
+      var curPois = $scope.getCurPois();
+      if (!curPois || curPois.length === 0) {
+        _.forEach($scope.pois, function(poi) {
+          poi.dist = 0;
+        });
+        return;
+      }
+      var lastPoi = curPois[curPois.length - 1];
+
+      function getDistanceFromLatLon(lat1, lon1, lat2, lon2) {
+        var R = 6371; // Radius of the earth in km
+        var dLat = deg2rad(lat2 - lat1); // deg2rad below
+        var dLon = deg2rad(lon2 - lon1);
+        var a =
+          Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+          Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) *
+          Math.sin(dLon / 2) * Math.sin(dLon / 2);
+        var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+        var d = R * c * 0.621371; // Distance in miles
+        return d;
+      }
+
+      function deg2rad(deg) {
+        return deg * (Math.PI / 180)
+      }
+
+      _.forEach($scope.pois, function(poi) {
+        poi.dist = getDistanceFromLatLon(poi.map_pos_lat, poi.map_pos_lng, lastPoi.map_pos_lat, lastPoi.map_pos_lng);
+      });
+    };
+
+    $scope.addCurrentDay = function(data, event) {
+      var curPois = $scope.getCurPois();
+      if (curPois) {
         var lastPoi = curPois[curPois.length - 1];
       }
       curPois.push(data);
@@ -61,11 +99,13 @@ angular.module('tripmakerApp')
           map: map
         });
       }
+      $scope.updateDistances();
     };
 
     $http.get('/pois/' + $scope.settings.destination).then(function(data) {
       $scope.pois = data.data.pois;
       $scope.map.center.latitude = data.data.loc.lat;
       $scope.map.center.longitude = data.data.loc.lng;
+      $scope.updateDistances();
     });
   });
